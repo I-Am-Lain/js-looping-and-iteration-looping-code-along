1e0f4566f7ce5fc347e48a38a2835d2a87f9fd7d
I-Am-Lain <vetter.william@gmail.com>
2020-10-26 01:22:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let thankYe = []
+    for (let i = 0; i < names.length; i++) {
+        thankYe.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYe;
+}
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+
+    }
+
+}
